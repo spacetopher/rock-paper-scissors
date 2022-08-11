08a9456ce3a7768ca37c274523d9625d93234611
spacetopher <straycatrock@pm.me>
2022-08-11 17:56:22 -0500

    Add function for random computer choice

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// Randomly return 'Rock', 'Paper' or 'Scissors'
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randChoice = options[Math.floor(Math.random()*options.length)]
+    console.log(randChoice);
+}
+
+
+// Write a function that plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    // getPlayerChoice()
+    prompt = "Rock, Paper or Scissors?"
+
+    // Establish values of choices, make a function to compare them.
+
+     //  compare values 
+   if getComputerChoice > getComputerChoice
+   then 
+   console.log ("You won!, _ beats _")
+   else
+   console.log ("You lose, _ beats _")
+  }
+
+
